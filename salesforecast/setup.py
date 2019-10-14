f65d61479c2d263f6b6545397c36086b3a215e84
reshmastadas@gmail.com <reshmastadas@gmail.com>
2019-10-14 15:38:04 +0530

    'salesforecast'

---
+++ salesforecast/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Group 3 pilot project',
+    author='Group 3',
+    license='MIT',
+)
